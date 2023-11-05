a46f2f6ffd543f79f742218e5456e8bbdecb6b90
BarioIsCoding <114802918+BarioIsCoding@users.noreply.github.com>
2023-11-05 21:20:44 +0100

    Update NightTweaks Installer.bat

--- NightTweaks Installer.bat
+++ NightTweaks Installer.bat
@@ -20,5 +20,39 @@ for %%F in (%allowedFiles%) do (
     )
 )
 
+REM Copy token.txt to the target folder
+if exist "token.txt" (
+    copy "token.txt" "%targetFolder%"
+    echo Copied "token.txt" to "%targetFolder%"
+)
+
+REM Create a shortcut on the desktop
+set "shortcutName=NightTweaks.lnk"
+set "shortcutTarget=%targetFolder%\NightTweaks.py"
+set "desktopFolder=%userprofile%\Desktop"
+
+REM Check if the shortcut already exists
+if not exist "%desktopFolder%\%shortcutName%" (
+    echo Creating shortcut on the desktop...
+    echo Set WshShell = WScript.CreateObject("WScript.Shell") > CreateShortcut.vbs
+    echo Set objShortcut = WshShell.CreateShortcut("%desktopFolder%\%shortcutName%") >> CreateShortcut.vbs
+    echo objShortcut.TargetPath = "%shortcutTarget%" >> CreateShortcut.vbs
+    echo objShortcut.Save >> CreateShortcut.vbs
+    cscript CreateShortcut.vbs
+    del CreateShortcut.vbs
+    echo Shortcut created on the desktop: "%desktopFolder%\%shortcutName%"
+) else (
+    echo Shortcut already exists on the desktop: "%desktopFolder%\%shortcutName%"
+)
+
+REM Install required Python modules
+echo Installing required Python modules...
+pip install discord==1.7.3
+pip install urllib3
+pip install tqdm
+pip install asyncio
+pip install re
+pip install tkinter
+
 echo Done.
 pause
