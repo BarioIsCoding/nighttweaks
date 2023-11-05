f5b5ee3ad4fc61c52105b64447d9afd201df8bb8
BarioIsCoding <114802918+BarioIsCoding@users.noreply.github.com>
2023-11-05 20:58:43 +0100

    Update NightTweaks Installer.bat

--- NightTweaks Installer.bat
+++ NightTweaks Installer.bat
@@ -1,2 +1,24 @@
 @echo off
-echo Lorem ipsum, not done..
+setlocal
+
+REM Define the target folder path
+set "targetFolder=C:\Users\%USERNAME%\AppData\Roaming\!!NightTweaks"
+
+REM Create the target folder if it doesn't exist
+if not exist "%targetFolder%" (
+    mkdir "%targetFolder%"
+)
+
+REM Define the list of allowed file names
+set "allowedFiles=EmbedGenerator.html NightTweaks Installer.bat NightTweaks.py README.md index.html"
+
+REM Loop through the files in the current directory
+for %%F in (%allowedFiles%) do (
+    if exist "%%~F" (
+        move "%%~F" "%targetFolder%"
+        echo Moved "%%~F" to "%targetFolder%"
+    )
+)
+
+echo Done.
+pause
